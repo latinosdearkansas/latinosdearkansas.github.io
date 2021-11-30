43b6cbbd860dfa1cc52413d00654205793c5594a
Rogelio Mojica <rogelio.mojica@outlook.com>
2021-11-29 21:23:54 -0600

    included changes to the pictures to create a slide show with javascript

--- scripts/script.js
+++ scripts/script.js
@@ -7,3 +7,32 @@ function hideMenu () {
 function showMenu () {
     navLinks.style.left= '-10px';
 }
+
+
+
+
+
+const mainLink = document.querySelector('.main-image');
+const mainImage = document.querySelector('.main-image > img');
+const originalText = document.querySelector('.original-text > a');
+
+const replacementLink = document.querySelectorAll('.replacement-image');
+const replacementImage = Array.from(document.querySelectorAll('.replacement-image > img'));
+const replacementText = document.querySelectorAll('.replacement-text > a');
+
+
+let i = 0;
+
+setInterval(function() {
+    
+    if (i < replacementImage.length) {
+        mainLink.href = replacementLink[i].href;
+        mainImage.src = replacementImage[i].src;
+        originalText.textContent = replacementText[i].textContent;
+        originalText.href = replacementText[i].href;
+        i++;
+    } else {
+        i = 0;
+    }
+    
+}, 6000)
