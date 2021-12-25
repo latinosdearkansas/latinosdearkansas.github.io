45f69239afa780414db6c9b3ba7b9538d91ac715
Rogelio Mojica <rogelio.mojica@outlook.com>
2021-12-25 12:38:08 -0600

    changes to main news and to adverstisement

--- scripts/script.js
+++ scripts/script.js
@@ -40,6 +40,54 @@ setInterval(function() {
 
 
 // -----  Patrocinadores  -----
-const patrocinadoresMainLink = document.querySelectorAll('.ventas > .section > a');
-const patrocinadoresMainImage = document.querySelectorAll('.ventas > .section > a > img');
+const patrocinadoresMainLink = Array.from(document.querySelectorAll('.ventas > .section > a'));
+const patrocinadoresMainImage = Array.from(document.querySelectorAll('.ventas > .section > a > img'));
 
+const patrocinadoresReplacementImage = [
+    'deep-performance',
+    'audi',
+    'beach',
+    'chita',
+    'lambo',
+    'mustang',
+    'space',
+    'rocks',
+    'paisaje',
+    'technology'
+]
+
+const patrocinadoresReplacementLink = [
+    'https://latinosdearkansas.github.io/deepperformancecustoms/',
+    'patrocinadores/index.html'
+]
+
+//fix code so page will only display blank if the page is in another webpage
+
+function advertisementOne() {
+    if (patrocinadoresMainLink !== null) {  //  <---- fix this to work in patrocinadores page
+        setInterval(() => {
+            if (i < patrocinadoresReplacementImage.length) {
+                let randomImage = Math.floor(Math.random() * patrocinadoresReplacementImage.length);
+                patrocinadoresMainImage[0].src = `../images/patrocinadores/${patrocinadoresReplacementImage[randomImage]}.jpg`;
+                patrocinadoresMainLink[0].href = patrocinadoresReplacementLink[randomImage];
+                patrocinadoresMainLink[0].target = '_blank';
+            }
+        }, 7000)
+    }
+}
+
+function advertisementTwo() {
+    if (patrocinadoresMainLink !== null) {
+        setInterval(() => {
+            if (i < patrocinadoresReplacementImage.length) {
+                let randomImage = Math.floor(Math.random() * patrocinadoresReplacementImage.length);
+                patrocinadoresMainImage[1].src = `../images/patrocinadores/${patrocinadoresReplacementImage[randomImage]}.jpg`;
+                patrocinadoresMainLink[1].href = patrocinadoresReplacementLink[randomImage];
+                patrocinadoresMainLink[1].target = '_blank';
+            }
+        }, 7000)
+    }
+}
+
+advertisementOne();
+advertisementTwo();
